b0a93f57095946cc8d8bf102d3c3311956100df8
Fabs Adetomiwa <fabikun.adetomiwa95@gmail.com>
2022-09-12 10:01:40 -0700

    Q7

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,10 +16,10 @@ int main(void)
 /**
  * size of is used to get the size of variables
  */
-	printf("size of char: %lu bytes\n", sizeof(charType));
-	printf("size of int: %lu bytes\n", sizeof(intType));
-	printf("size of long int: %lu bytes\n", sizeof(longintType));
-	printf("size of long long int: %lu bytes\n", sizeof(LLintType));
-	printf("size of float: %lu bytes\n", sizeof(floatType));
+	printf("size of char: %ld bytes\n", sizeof(charType));
+	printf("size of int: %ld bytes\n", sizeof(intType));
+	printf("size of long int: %ld bytes\n", sizeof(longintType));
+	printf("size of long long int: %ld bytes\n", sizeof(LLintType));
+	printf("size of float: %ld bytes\n", sizeof(floatType));
 		return (0);
 }
