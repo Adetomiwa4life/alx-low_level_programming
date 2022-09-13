422463dec5ece0ac8aa6785f7214609d503145b1
Fabs Adetomiwa <fabikun.adetomiwa95@gmail.com>
2022-09-12 20:01:23 -0700

    Q12

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <sdlib.h>
+
+/**
+ *
+ * main - print posssible combo of two digits
+ * Description: print 
+ */
+int main(void)
+{
+	int a = 0, b;
+	while(a <= 9)
+	{
+		b = 0;
+		while(b <= 9)
+		{
+			if(a !=b && a < b)
+		{	
+			putchar(a + 48);
+			putchar(b + 48);
+
+			if(a + b != 17)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		++b;
+		}
+		++a;
+	}
+	
+	putchar('\n');
+	return 0;
+}
