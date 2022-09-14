15f4cd7a7d376555860c066bc9059d24bba48d58
Fabs Adetomiwa <fabikun.adetomiwa95@gmail.com>
2022-09-13 19:13:04 -0700

    Write a program that prints _putchar, followed by a new line

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,18 +4,18 @@
 /**
  *
  * main - print posssible combo of two digits
- * Description: print 
+ * Description: print
  */
 int main(void)
 {
 	int a = 0, b;
-	while(a <= 9)
+	while (a <= 9)
 	{
 		b = 0;
 		while(b <= 9)
 		{
 			if(a !=b && a < b)
-		{	
+		{
 			putchar(a + 48);
 			putchar(b + 48);
 
